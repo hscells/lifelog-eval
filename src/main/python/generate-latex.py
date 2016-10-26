f24d9b01da98bb3df36e79489782f3bce9cf2819
Harry Scells <harryscells@gmail.com>
2016-10-26 20:09:12 +1000

    better table layout and a graph

--- src/main/python/generate-latex.py
+++ src/main/python/generate-latex.py
@@ -9,6 +9,8 @@ import os
 import pg8000
 import sys
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def format_time(td):
@@ -26,6 +28,52 @@ def format_time(td):
     return '{} seconds'.format(td.seconds)
 
 
+def annotators_breakdown(cursor, file):
+    """
+
+    :param cursor:
+    :param file:
+    :return:
+    """
+    sql = '''
+SELECT row_number() OVER() AS annotator, sum(
+    (SELECT count(DISTINCT image_id) FROM annotated_text_images WHERE person_id = p.id) +
+    (SELECT count(DISTINCT image_id) FROM annotated_tag_images WHERE person_id = p.id) +
+    (SELECT count(DISTINCT image_id) FROM annotated_query_images WHERE person_id = p.id) +
+    (SELECT count(DISTINCT image_id) FROM annotated_assessment_images WHERE person_id = p.id)
+) total
+FROM people p
+GROUP BY p.alias
+ORDER BY total DESC;
+    '''
+
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    count = []
+    total = 0
+    for row in results:
+        if row[1] > 0:
+            total += row[1]
+            count.append(row[1])
+
+    ind = np.arange(len(count))
+    width = 1.0
+    fig, ax = plt.subplots()
+    rects = ax.bar(ind, count, width)
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(ind + 1, minor=False)
+    plt.title('Annotation Count Breakdown')
+    plt.xlabel('Annotator (anonymised)')
+    plt.ylabel('Number of annotations')
+    plt.savefig(file)
+    return total
+
 
 def annotators_table(cursor):
     """
@@ -37,7 +85,7 @@ def annotators_table(cursor):
 \\begin{center}
     \\begin{tabular}{ | l | l | l | l | p{5cm} |}
     \hline
-    & Text & Tags & Query & Relevance Assessment \\\\ \hline
+    Name & Count & Average Time & Total Time \\\\ \hline
     '''
     table_end = '''
     \end{tabular}
@@ -45,45 +93,34 @@ def annotators_table(cursor):
     '''
 
     queries = [
-        'SELECT count(id), avg(end_time - start_time), sum(end_time - start_time) '
-        'FROM annotated_text_images;',
-        'SELECT count(id), avg(end_time - start_time), sum(end_time - start_time) '
-        'FROM annotated_tag_images;',
-        'SELECT count(id), avg(end_time - start_time), sum(end_time - start_time) '
-        'FROM annotated_query_images;',
-        '''SELECT count(DISTINCT image_id), avg(a.end_time - a.start_time), q.z
-           FROM annotated_assessment_images a,
-           (SELECT DISTINCT sum(x.y) z FROM
-            (SELECT DISTINCT image_id, avg(end_time - start_time) y
-             FROM annotated_assessment_images GROUP BY image_id) x) q
-           GROUP BY q.z;'''
+        ('Text', 'SELECT count(id), avg(end_time - start_time), sum(end_time - start_time) '
+                 'FROM annotated_text_images;'),
+        ('Tag', 'SELECT count(id), avg(end_time - start_time), sum(end_time - start_time) '
+                'FROM annotated_tag_images;'),
+        ('Query', 'SELECT count(id), avg(end_time - start_time), sum(end_time - start_time) '
+                  'FROM annotated_query_images;'),
+        ('Assessment', '''SELECT count(DISTINCT image_id), avg(a.end_time - a.start_time), q.z
+                          FROM annotated_assessment_images a,
+                           (SELECT DISTINCT sum(x.y) z FROM
+                            (SELECT DISTINCT image_id, avg(end_time - start_time) y
+                             FROM annotated_assessment_images GROUP BY image_id) x) q
+                          GROUP BY q.z;''')
     ]
 
     query_results = []
-
-    for query in queries:
+    total_count, total_time = 0, 0
+    for name, query in queries:
         cursor.execute(query)
         result = cursor.fetchone()
-        query_results.append((format_time(result[1]), result[0], format_time(result[2])))
+        query_results.append((name, format_time(result[1]), result[0], format_time(result[2])))
 
     print(query_results)
 
-    count = 'Count & {} & {} & {} & {} \\\\ \hline'.format(query_results[0][1],
-                                                           query_results[1][1],
-                                                           query_results[2][1],
-                                                           query_results[3][1])
-    avg_time = 'Average Time & {} & {} & {} & {} \\\\ \hline'.format(query_results[0][0],
-                                                                     query_results[1][0],
-                                                                     query_results[2][0],
-                                                                     query_results[3][0])
-
-    total_time = 'Total Time & {} & {} & {} & {} \\\\ \hline'.format(query_results[0][2],
-                                                                     query_results[1][2],
-                                                                     query_results[2][2],
-                                                                     query_results[3][2])
-
-    return '{0}{1}\n    {2}\n    {3}{4}'.format(table_start, count, avg_time, total_time,
-                                                table_end)
+    return '{}{}{}'.format(table_start,
+                           '\n    '.join(['{} & {} & {} & {} \\\\ \hline'.format(a, c, b, d)
+                                          for (a, b, c, d) in
+                                          query_results]),
+                           table_end)
 
 
 def trec_eval_table(results):
@@ -127,6 +164,9 @@ if __name__ == '__main__':
     argparser.add_argument('--annotations_table',
                            help='table showing the number of annotations',
                            required=False, default=False, type=bool)
+    argparser.add_argument('--annotator_breakdown',
+                           help='table showing the number of annotations',
+                           required=False, default=None, type=str)
     argparser.add_argument('--trec_eval_results',
                            help='table showing a trec_eval results',
                            required=False, type=argparse.FileType('r'))
@@ -134,9 +174,9 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     if os.environ.get('LIFELOG_DB_USER') is None or \
-                    os.environ.get('LIFELOG_DB_PASS') is None or \
-                    os.environ.get('LIFELOG_DB_HOST') is None or \
-                    os.environ.get('LIFELOG_DB_NAME') is None:
+       os.environ.get('LIFELOG_DB_PASS') is None or \
+       os.environ.get('LIFELOG_DB_HOST') is None or \
+       os.environ.get('LIFELOG_DB_NAME') is None:
         print('Environment variables are not set!')
         sys.exit(1)
 
@@ -154,5 +194,7 @@ if __name__ == '__main__':
         run_arg(annotators_table(cursor), 'annotations table')
     if args.trec_eval_results is not None:
         run_arg(trec_eval_table(args.trec_eval_results.read()), 'trec_eval results')
+    if args.annotator_breakdown is not None:
+        run_arg(annotators_breakdown(cursor, args.annotator_breakdown), 'annotator breakdown')
 
     conn.close()
