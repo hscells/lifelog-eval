16b662c0f11d62559a6876da863035702337f9bf
Harry Scells <harryscells@gmail.com>
2016-10-26 20:09:31 +1000

    don't limit transformation to just text

--- src/main/python/transform-captions.py
+++ src/main/python/transform-captions.py
@@ -7,7 +7,7 @@ import sys
 import json
 
 
-def transform(captions):
+def transform(captions, name):
     captions = json.loads(captions.read())
     annotations = []
     for image_id, caption in captions.items():
@@ -16,10 +16,9 @@ def transform(captions):
             caption = caption.replace('bluered', 'red blue')
             caption = caption.replace('presentinglecturing', 'presenting lecturing')
             caption = caption.replace('lecturingpresenting', 'presenting lecturing')
-            annotations.append({'id': image_id, 'annotations': {'text': caption,
-                                                                'tags': None,
-                                                                'assessments': None,
-                                                                'query': None}})
+            annotation_types = dict(text=None, tags=None, assessments=None, query=None)
+            annotation_types[name] = caption
+            annotations.append({'id': image_id, 'annotations': annotation_types})
     return json.dumps(annotations, sort_keys=False, indent=2, separators=(',', ': '))
 
 
@@ -29,6 +28,8 @@ if __name__ == '__main__':
                            default=sys.stdin, type=argparse.FileType('r'), required=False)
     argparser.add_argument('-o', '--output', help='output file',
                            default=sys.stdout, type=argparse.FileType('w'), required=False)
+    argparser.add_argument('-n', '--name', help='the annotation name', required=True, type=str,
+                           choices=['text', 'tags', 'query', 'assessments'])
 
     args = argparser.parse_args()
-    args.output.write(transform(args.captions))
+    args.output.write(transform(args.captions, args.name))
